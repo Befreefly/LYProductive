d09033aa24c08d7bb8d85c5c5482ce9ccc04b134
LYC <LYCMAC@huangninghaideimac.local>
2018-01-16 21:13:21 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by LYC on 2018/1/16.
+//  Copyright © 2018年 YCLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
